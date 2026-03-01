e514bd5594ba9fa58474f46079db26fa8a6ae90f
Ibrahim Tajudeen <donslice6@gmail.com>
2026-03-01 04:12:09 +0100

    add: site icon

--- app/head.tsx
+++ app/head.tsx
@@ -11,8 +11,8 @@ export default function Head() {
       />
 
       {/* Favicons */}
-      <link rel="icon" href="/img/favicon.png" />
-      <link rel="apple-touch-icon" href="/img/apple-touch-icon.png" />
+      <link rel="icon" href="/favicon.png" />
+      <link rel="apple-touch-icon" href="/favicon.png" />
 
       {/* Google Fonts */}
       <link rel="preconnect" href="https://fonts.googleapis.com" />
